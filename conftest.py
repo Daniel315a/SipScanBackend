a37fa1b330dfcf04116c6a72fa9bd2cc7d733d14
daniellopez <daniel315.a@gmail.com>
2026-03-04 06:26:27 -0500

    🧪 Add test suite with pytest setup and initial receipt service tests

---
+++ conftest.py
@@ -0,0 +1,6 @@
+import os
+
+os.environ.setdefault("GEMINI_API_KEY", "dummy")
+os.environ.setdefault("ERP_DOCUMENTOS_HOST", "dummy")
+os.environ.setdefault("ERP_TERCEROS_HOST", "dummy")
+os.environ.setdefault("ERP_API_TOKEN", "dummy")
